760c961b048a6fb3e1ac999644afe542e2062d91
Jaime Luong <jaime.luong@gmail.com>
2023-01-22 00:51:07 -0600

    Fix Devise bug with Turbo, add sign-in and sign-out links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root to: 'posts#index'
 
   resources :posts, only: [:new, :create, :index]
 end
